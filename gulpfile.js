1e5c2dcdf3b2c80c07ba9baa384c2b310343a176
Brandon Pierce <brandonpierce91@gmail.com>
2014-02-26 17:07:54 -0500

    completely redid the build system, now gulp based

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,87 @@ var gutil = require('gulp-util');
 var sass = require('gulp-sass');
 var prefix = require('gulp-autoprefixer');
 var rename = require('gulp-rename');
+var concat = require('gulp-concat');
+
+var config = {
+  attention: {
+    bounce: true,
+    hang: true,
+    hover: true,
+    shake: true
+  },
+  bounce: {
+    bounceInDown: true,
+    bounceInLeft: true,
+    bounceInRight: true,
+    bounceInUp: true,
+    bounceOutDown: true,
+    bounceOutLeft: true,
+    bounceOutRight: true,
+    bounceOutUp: true
+  },
+  fade: {
+    fadeIn: true,
+    fadeInDown: true,
+    fadeInLeft: true,
+    fadeInRight: true,
+    fadeInUp: true,
+    fadeOut: true,
+    fadeOutDown: true,
+    fadeOutLeft: true,
+    fadeOutRight: true,
+    fadeOutUp: true
+  },
+  slide: {
+    slideInDown: true,
+    slideInLeft: true,
+    slideInRight: true,
+    slideInUp: true,
+    slideOutDown: true,
+    slideOutLeft: true,
+    slideOutRight: true,
+    slideOutUp: true
+  },
+  misc: {
+    fallIn: true,
+    fallOut: true,
+    flipInHorizontal: true,
+    flipOutHorizontal: true,
+    flipInVertical: true,
+    flipOutVertical: true,
+    popIn: true,
+    popOut: true,
+    scaleIn: true,
+    scaleOut: true
+  }
+}
+
+var cssSrc = ['./src/scss/alive.scss'];
+
+for (var cat in config) {
+  for (var file in config[cat]) {
+    if (config[cat][file]) {
+      cssSrc.push('./src/scss/modules/' + cat + '/' + file + '.scss');
+    }
+  }
+}
 
 gulp.task('sass', function () {
-  return gulp.src('./src/scss/alive.scss')
+  return gulp.src(cssSrc)
+    .pipe(concat('concat.scss'))
+    .pipe(gulp.dest('./src/scss'))
     .pipe(sass({
-      outputStyle: 'compressed'
+      outputStyle: 'compressed',
+      includePaths: './src/scss/concat.scss',
     }))
-    .pipe(prefix("last 2 version", "ie 9"))
+    .pipe(prefix('last 3 version', 'ie 9', 'bb 10', 'android 3'))
     .pipe(rename('alive.min.css'))
     .pipe(gulp.dest('./dist/'));
 });
 
 gulp.task('watch', function() {
-  gulp.watch(['./src/scss/*.scss', './src/scss/**/*.scss'], ['sass']);
+  gulp.watch(cssSrc, ['sass']);
 })
 
-gulp.task('default', ['sass', 'watch']);
+gulp.task('default', ['sass']);
+gulp.task('dev', ['sass', 'watch']);
